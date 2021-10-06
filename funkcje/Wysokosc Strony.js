40051239c417660239bcd0c2a9aa148547959e21
olidiaks <twoja@torzsamo.com>
2021-10-06 20:11:50 +0200

    Dodanie sprawdzania czy strona nie jest wyższa niż wypełnione ciał danej strony.)

--- funkcje/Wysokosc Strony.js
+++ funkcje/Wysokosc Strony.js
@@ -1,4 +1,8 @@
 export const ustawWysokośćElementuTakąJakStrony = element => {
     const wysokośćStrony = $(window).height();
-    $(element).css("height", wysokośćStrony);
+    const wysokośćCiała = $("body").height();
+
+    if (wysokośćCiała < wysokośćStrony){
+        $(element).css("height", wysokośćStrony);
+    }
 }
